cb004423051380a2ccc3965a196c438dd76fa8cf
Marina <linamamarinina@gmail.com>
2022-07-14 10:10:43 +0300

    Task 10. Draft

---
+++ 2_HomeWork/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+// Зад. 10: Программа принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+int TheSecondDigit(int num)
+{
+    int dig = num / 10 % 10;
+
+    return dig;
+}
+
+Console.Write("Input a three-digit integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit = TheSecondDigit(number);
+
+Console.WriteLine($"The second digit is {digit}");
+*/
+
+// Зад. 13: Программа выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+
+
+// Зад. 15: Программа принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
