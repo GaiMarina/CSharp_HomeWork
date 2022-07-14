32401df3b305aa99aa70712b9cce4fe18715d3c5
Marina <linamamarinina@gmail.com>
2022-07-14 13:56:50 +0300

    Task 13. Draft

--- 2_HomeWork/Program.cs
+++ 2_HomeWork/Program.cs
@@ -20,6 +20,37 @@ Console.WriteLine($"The second digit is {digit}");
 // Зад. 13: Программа выводит третью цифру заданного числа или сообщает, 
 // что третьей цифры нет.
 
+int TheThirdDigit(int num)
+{
+    int d = num % 10;
+
+    return d;
+}
+
+int ThirdDigitNumber(int n)
+{
+    while(n > 999)
+    n /= 10;
+
+    return n;
+}
+
+Console.Write("Input an integer number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int tdn = ThirdDigitNumber(number);
+
+if(tdn > 99)
+{
+    int dig = TheThirdDigit(tdn);
+    Console.WriteLine($"The third digit is {dig}");
+}
+else
+    Console.WriteLine($"The third digit is missing.");
+    
+
+
+
 
 
 // Зад. 15: Программа принимает на вход цифру, обозначающую день недели, и проверяет, 
