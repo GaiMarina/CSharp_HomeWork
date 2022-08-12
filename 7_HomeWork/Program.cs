d8c74edaa8daa6a4d46b4453a24f45a9bba84b39
Marina <linamamarinina@gmail.com>
2022-08-12 19:27:55 +0300

    Task n.50.Поиск заданного элемента

--- 7_HomeWork/Program.cs
+++ 7_HomeWork/Program.cs
@@ -1,14 +1,6 @@
-﻿
+﻿/*
 // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными 
 //числами.
-
-//m = 3, n = 4.
-
-//0,5 7 -2 -0,2
-
-//1 -3,3 8 -9,9
-
-//8 7,8 -7,1 9
 
 double[,] CreateDouble2dArray(int rows, int columns, int minValue, int maxValue)
 {
@@ -37,7 +29,7 @@ double[,] CreateDouble2dArray(int rows, int columns, int minValue, int maxValue)
         Console.WriteLine();
     }
  }
-/*
+
 Console.Write("Input the number of rows ");
 int r = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input the number of columns ");
@@ -55,36 +47,25 @@ Show2dArray(FinalArray);
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
-/*
-Например, задан массив:
 
-1 4 7 2
-
-5 9 2 3
-
-8 4 2 4
-
-1,7 -> такого числа в массиве нет
-*/
-double[,] CreateDouble2dArray(int rows, int columns)
+int[,] CreateRandom2dArray(int rows, int columns) 
 {
-    double[,] newArray = new double[rows,columns];
-    Random rand = new Random();
+    int[,] newArray = new int[rows,columns];
 
     for(int i = 0; i < rows; i++)
     {
         for(int j = 0; j < columns; j++)
-        {   
-            newArray[i,j] = rand.Next() + rand.NextDouble();                   
+        {
+            newArray[i,j] = new Random().Next(1,10);
         }
     }
- 
+    
     return newArray;
 }
 
- void Show2dArray(double[,] array)
+void Show2dArray(int[,] array)
  {
-    for(int i = 0; i < array.GetLength(0); i++)
+    for(int i = 0; i < array.GetLength(0); i++) 
     {
         for(int j = 0; j < array.GetLength(1); j++)
         {
@@ -94,47 +75,39 @@ double[,] CreateDouble2dArray(int rows, int columns)
     }
  }
 
-string FindElementValue(int row, int column, double[,] array)
+void FindElementValue(int[,] array, int row, int column)
 {
-    for(int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if(i = row; && j = column)
-        }
-        
-    }
+    if(row < array.GetLength(0) && column < array.GetLength(1))
+        Console.WriteLine($"The corresponding element is {array[row, column]}");
+    else    
+        Console.WriteLine("The element is out of matrix.");        
 }
 
-
-Console.Write("Input the number of rows ");
+Console.Write("Input the quantity of rows ");
 int r = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input the number of columns ");
+Console.Write("Input the quantity of columns ");
 int col = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Input the number of row to find ");
-int r = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the number of row to find ");
+int hor = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Input the number of column to find ");
-int col = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the number of column to find ");
+int vert = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 
-double[,] FinalArray = CreateDouble2dArray(r, col, min, max);
-Show2dArray(FinalArray);
-
-Console.WriteLine("Input the number of row to find ");
-int r = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Input the number of column to find ");
-int col = Convert.ToInt32(Console.ReadLine());
-
+int[,] newArray = CreateRandom2dArray(r, col);
+Show2dArray(newArray);
+Console.WriteLine();
+FindElementValue(newArray, hor, vert);
 
 //=================================
 
-// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-/*
-Например, задан массив:
+// Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+/*Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-*/
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+
