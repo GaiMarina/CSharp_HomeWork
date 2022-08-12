450d18907e87c5cc244a7de3c0d198cc76e4a5e0
Marina <linamamarinina@gmail.com>
2022-08-12 20:47:22 +0300

    Task n. 52 Среднее арифметическое

--- 7_HomeWork/Program.cs
+++ 7_HomeWork/Program.cs
@@ -105,11 +105,6 @@ FindElementValue(newArray, hor, vert);
 
 // Задача 52. Задайте двумерный массив из целых чисел. 
 // Найдите среднее арифметическое элементов в каждом столбце.
-/*Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
 
 int[,] CreateRandom2dArray(int rows, int columns) 
 {
@@ -144,7 +139,7 @@ void ArithmeticalMean(int[,] array)
     {   
         double sum = 0;
         double count = 0;
-        
+
         for(int i = 0; i < array.GetLength(0); i++)
         {
             sum += array[i,j];
